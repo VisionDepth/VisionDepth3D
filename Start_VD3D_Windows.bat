41ac5e806d6f352ae71db42459163c57365770ea
Johnathan Carpenter <Redsky90@gmail.com>
2025-04-10 22:12:49 -0400

    Update Start_VD3D_Windows.bat
    updated windows startup script

--- Start_VD3D_Windows.bat
+++ Start_VD3D_Windows.bat
@@ -1,8 +1,10 @@
 @echo off
-cd /d "Path\to\VisionDepth3D-main" #Update path to VisionDepth3D folder
+cd /d "E:\Video Editing\VisionDepth3D-main"
 call VD3D_env\Scripts\activate
-python "D:\Video Editing\VisionDepth3D-main\core\GUI.py"
+python "E:\Video Editing\VisionDepth3D-main\core\GUI.py"
 call VD3D_env\Scripts\deactivate
 #DeleteMe2keepWindowOpen pause
+#Replace Both lines in quotes with your actual path to Main folder and GUI.py
+#Can delete both calls if not using a virtual environment
 
 #thank you Ryudoadema for providing the windows startup script
