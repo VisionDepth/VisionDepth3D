92ce1755e4ad20cee16fb12e0370e7301cc698b4
Johnathan Carpenter <Redsky90@gmail.com>
2025-04-28 22:17:15 -0400

    Update Start_VD3D_Conda.bat
    cleaned up conda batch script

--- Start_VD3D_Conda.bat
+++ Start_VD3D_Conda.bat
@@ -1,8 +1,7 @@
 @echo off
 cd /d "%~dp0"
-call conda activate VD3D #change to created VisionDepth3D Environment
-echo ✅ Starting VisionDepth3D in current Conda environment: %CONDA_DEFAULT_ENV% 
+:: Change the call under this line to the created VisionDepth3D Environment
+call conda activate VD3D
+echo ✅ Starting VisionDepth3D in current Conda environment: %CONDA_DEFAULT_ENV%
 python core\GUI.py
 pause
-
-#Huge thank you to Aether for adding line 3 to finish the script
