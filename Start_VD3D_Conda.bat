53637bd7bbbb1727eb5f5d025762c011f15f8c6f
Johnathan Carpenter <Redsky90@gmail.com>
2025-05-03 11:47:14 -0400

    Update Start_VD3D_Conda.bat
    changed name from GUI.py to VisionDepth3D.py to work with new script

--- Start_VD3D_Conda.bat
+++ Start_VD3D_Conda.bat
@@ -3,5 +3,5 @@ cd /d "%~dp0"
 :: Change the call under this line to the created VisionDepth3D Environment
 call conda activate VD3D
 echo ✅ Starting VisionDepth3D in current Conda environment: %CONDA_DEFAULT_ENV%
-python core\GUI.py
+python VisionDepth3D.py
 pause
