50fbc8c05c096c63e5bbbf06990c93fdcaf08ae1
Johnathan Carpenter <Redsky90@gmail.com>
2025-05-28 23:52:36 -0400

    Update VisionDepth3D.py
    forgot to take out DLL directory

--- VisionDepth3D.py
+++ VisionDepth3D.py
@@ -107,17 +107,17 @@ core_dir = resource_path("core")
 if core_dir not in sys.path:
     sys.path.insert(0, core_dir)
 
-    
+# --INNO SETUP STUFF -- 
 # ✅ Inject DLL directory into PATH
-def inject_dll_directory():
-    dll_dir = resource_path("dlls")
-    if os.path.isdir(dll_dir):
-        os.environ["PATH"] = dll_dir + os.pathsep + os.environ.get("PATH", "")
-    else:
-        print(f"[Warning] DLL folder not found: {dll_dir}")
+#def inject_dll_directory():
+#   dll_dir = resource_path("dlls")
+ #   if os.path.isdir(dll_dir):
+ #       os.environ["PATH"] = dll_dir + os.pathsep + os.environ.get("PATH", "")
+ #   else:
+ #       print(f"[Warning] DLL folder not found: {dll_dir}")
 
 # 🟢 Call this before any ONNX/TensorRT/CUDA init
-inject_dll_directory()
+#inject_dll_directory()
 
 
 def save_settings():
