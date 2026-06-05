5ff627ea5f589c79dd23db075a5427e491f65a3a
Johnathan Carpenter <Redsky90@gmail.com>
2026-06-05 08:57:48 -0400

    Delete Start_VD3D_Conda.bat

--- Start_VD3D_Conda.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "%~dp0"
-:: Change the call under this line to the created VisionDepth3D Environment
-call conda activate VD3D
-echo ✅ Starting VisionDepth3D in current Conda environment: %CONDA_DEFAULT_ENV%
-python app.py
-pause
